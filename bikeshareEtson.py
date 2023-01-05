068e1db55ed940556484437a2d00842734f2e0b1
Etson33 <etson.banze@gmail.com>
2023-01-05 22:36:13 +0200

    feat: A new project bikeshareEtson.py

---
+++ bikeshareEtson.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+conda install -c defaults -c conda-forge ipynb-py-convert
+
+
+# In[ ]:
+
+
+pip install ipynb-py-convert
+
+
+# In[ ]:
+
+
+
+
